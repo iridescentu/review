2aafe940e25172e8b84c8ee0593bd83f9e7f9204
iridescentu <jihee.yoon.119@gmail.com>
2024-01-26 09:47:19 +0900

    AuthController updated

--- lms/src/main/java/project/lms/controller/AuthController.java
+++ lms/src/main/java/project/lms/controller/AuthController.java
@@ -27,6 +27,7 @@ import project.lms.jwt.TokenProvider;
 import project.lms.model.LoginHistory;
 import project.lms.model.Member;
 import project.lms.repository.LoginHistoryRepository;
+import project.lms.repository.MemberRepository;
 
 @RestController
 @RequestMapping("/api")
@@ -38,6 +39,12 @@ public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final LoginHistoryRepository loginHistoryRepository;  // LoginHistoryRepository 추가
 
+//    @Autowired
+//    private MemberService memberService;  // MemberService를 주입 받음
+    
+    @Autowired
+    private MemberRepository memberRepository;
+    
     @Autowired
     public AuthController(TokenProvider tokenProvider, 
                           AuthenticationManagerBuilder authenticationManagerBuilder, 
@@ -52,29 +59,32 @@ public class AuthController {
 	public ResponseEntity<ResponseDto<TokenDto>> authorize(@RequestBody @Valid MemberLoginDto memberLoginDto) {
 		
 		// 사용자 인증을 위한 토큰 생성
-		UsernamePasswordAuthenticationToken authenticationToken =
-				new UsernamePasswordAuthenticationToken(memberLoginDto.getLoginId(), memberLoginDto.getPassword());
-		
-		// 사용자 인증
-		Authentication authentication = authenticationManagerBuilder.getObject()
-				.authenticate(authenticationToken);
-		
-		// SecurityContext에 인증 정보 설정
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
-		// JWT 토큰 생성
-		String jwt = tokenProvider.createToken(authentication);
-		
-		// HTTP 헤더에 토큰 추가
-		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-		
-		// LoginHistory에서 받을 정보 추가 필요
-		// 로그인 이력 생성 및 저장
-	    LoginHistory loginHistory = new LoginHistory();
-	    loginHistory.setMember((Member)authentication.getPrincipal());  // 로그인한 Member 객체를 설정
-	    loginHistory.setLoginTime(LocalDateTime.now()); // 현재 시간을 로그인 시간으로 설정
-	    loginHistoryRepository.save(loginHistory); // 로그인 이력 저장
+				UsernamePasswordAuthenticationToken authenticationToken =
+						new UsernamePasswordAuthenticationToken(memberLoginDto.getLoginId(), memberLoginDto.getPassword());
+				
+				// 사용자 인증
+				Authentication authentication = authenticationManagerBuilder.getObject()
+						.authenticate(authenticationToken);
+				
+				// SecurityContext에 인증 정보 설정
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				
+				// JWT 토큰 생성
+				String jwt = tokenProvider.createToken(authentication);
+				
+				// HTTP 헤더에 토큰 추가
+				HttpHeaders httpHeaders = new HttpHeaders();
+				httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+				
+				// LoginHistory에서 받을 정보 추가 필요
+				// 로그인 이력 생성 및 저장
+			    LoginHistory loginHistory = new LoginHistory();
+			    Member member = memberRepository.findByLoginId(memberLoginDto.getLoginId());  // loginId를 사용하여 Member 객체 불러오기
+			    // 로그인 이력에 Member 객체를 설정
+			    loginHistory.setMember(member); 
+			    loginHistory.setLoginTime(LocalDateTime.now()); // 현재 시간을 로그인 시간으로 설정
+			    loginHistoryRepository.save(loginHistory); // 로그인 이력 저장
+ 
 		
 		// 클라이언트에게 토큰을 담은 응답을 반환
 		return new ResponseEntity<>(
